c376698624272b17aa92630e313f56c186d38063
shudal <shudal@qq.com>
2018-12-30 12:08:23 +0000

    完善

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,11 +10,29 @@ class Index extends Base
     }
     public function pv(){
       $list = scandir('/usr/share/nginx/html/tp5/public/static/pv');
-      unset($list[0]);unset($list[1]);
-
+      unset($list[0]);unset($list[1]);$list=array_slice($list,0);
+      return view('pv',['list'=>$list]);
+    }
+    public function pa($class,$start=0){
+      $list = scandir("/usr/share/nginx/html/tp5/public/static/pv/$class");
+      unset($list[0]);unset($list[1]);$list=array_slice($list,0); 
+      
       $size = count($list);
-      $start = empty(request()->param('start')) ? 0 : request()->param('start');
       $list = array_slice($list,$start,5);
-      return view('pv',['start'=>$start,'size'=>$size,'list'=>$list]);
+      return view('pa',['list'=>$list,'size'=>$size,'class'=>$class,'now_i'=>$start]);
+    }
+    public function pc(){
+      $list = scandir('/usr/share/nginx/html/tp5/public/static/pc');
+      unset($list[0]);unset($list[1]);$list=array_slice($list,0);
+      return view('pc',['list'=>$list]);
+    }
+    public function pi($class,$start=0){
+      $list  = scandir("/usr/share/nginx/html/tp5/public/static/pc/$class");
+      unset($list[0]);unset($list[1]);$list=array_slice($list,0);
+      
+      $size=count($list);
+      $list=array_slice($list,$start,5);
+
+      return view('pi',['list'=>$list,'size'=>$size,'class'=>$class,'now_i'=>$start]);
     }
 }
